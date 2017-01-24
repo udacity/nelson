0bbde12d1f71c6bec59b2f0505e5cdaee7e5fce9
CGearhart <cgearhart@gmail.com>
2017-01-23 21:42:31 -0500

    Fix bug causing left half of upload bar to grow at 2x speed.

--- nelson/uploadcallbacks.py
+++ nelson/uploadcallbacks.py
@@ -12,7 +12,7 @@ def progressbar_callback(monitor):
   total_bytes = monitor.encoder.len
   pct = float(monitor.bytes_read) / total_bytes
 
-  l_fill = "{:<27}".format("=" * min(int((min(0.5, pct) / 0.5) / 0.5 * 30), 27))
+  l_fill = "{:<27}".format("=" * min(int(min(0.5, pct) / 0.5 * 30), 27))
   p_fill = "{:^4}".format(str(int(pct * 100)) + "%")
   r_fill = "{:<27}".format("=" * min(int(min(pct - 0.5, 0.5) / 0.5 * 30), 27))
   ratio = "{!s}/{!s}".format(monitor.bytes_read, total_bytes)
